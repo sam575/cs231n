b15dc7defd3b079b5dd4a62d2b097d9d45398925
Rakhil Immidisetti <samrakhil@gmail.com>
2017-10-03 15:02:25 +0530

    Update linear_classifier.py

--- assignment1/cs231n/classifiers/linear_classifier.py
+++ assignment1/cs231n/classifiers/linear_classifier.py
@@ -53,6 +53,9 @@ class LinearClassifier(object):
       # Hint: Use np.random.choice to generate indices. Sampling with         #
       # replacement is faster than sampling without replacement.              #
       #########################################################################
+      ind = np.random.choice(num_train,batch_size)
+      X_batch = X[ind]
+      y_batch = y[ind]
       pass
       #########################################################################
       #                       END OF YOUR CODE                                #
@@ -67,6 +70,7 @@ class LinearClassifier(object):
       # TODO:                                                                 #
       # Update the weights using the gradient and the learning rate.          #
       #########################################################################
+      self.W = self.W - learning_rate*grad
       pass
       #########################################################################
       #                       END OF YOUR CODE                                #
@@ -96,6 +100,8 @@ class LinearClassifier(object):
     # TODO:                                                                   #
     # Implement this method. Store the predicted labels in y_pred.            #
     ###########################################################################
+    z = np.dot(X,self.W)
+    y_pred = np.max(z,axis=0)
     pass
     ###########################################################################
     #                           END OF YOUR CODE                              #
