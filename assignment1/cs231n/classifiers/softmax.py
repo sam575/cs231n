cf507d2ad801fa3ed068aa8d7e129cc5bba45267
Rakhil Immidisetti <samrakhil@gmail.com>
2017-10-12 00:02:59 +0530

    Update softmax.py

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -24,12 +24,35 @@ def softmax_loss_naive(W, X, y, reg):
   loss = 0.0
   dW = np.zeros_like(W)
 
+
   #############################################################################
   # TODO: Compute the softmax loss and its gradient using explicit loops.     #
   # Store the loss in loss and the gradient in dW. If you are not careful     #
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
+  
+  
+  for i in xrange(X.shape[0]):
+  	z = np.zeros(W.shape[1])
+  	for j in xrange(W.shape[1]):
+  		for k in xrange(X.shape[1]):
+  			z[j] += X[i][k]*W[k][j]
+  	max_ind = np.argmax(z)
+  	z = z - np.max(z)
+  	z = np.exp(z)
+  	z = z/np.sum(z)
+  	loss += -np.log(z[y[i]])
+  	z[y[i]] = z[y[i]] - 1
+  	for j in xrange(W.shape[1]):
+  		for k in xrange(X.shape[1]):
+  			dW[k][j] += X[i][k]*z[j]
+  loss = loss/X.shape[0]
+  dW = dW/X.shape[0]
+  for j in xrange(W.shape[1]):
+	for k in xrange(X.shape[1]):
+		loss += 0.5*reg*W[k][j]*W[k][j]
+		dW[k][j] += reg*W[k][j]
   pass
   #############################################################################
   #                          END OF YOUR CODE                                 #
@@ -54,6 +77,22 @@ def softmax_loss_vectorized(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
+  z = np.dot(X,W)
+  z = z.T - np.max(z,axis=1)
+  z = z.T
+  z = np.exp(z)
+  z = z/np.expand_dims(np.sum(z,axis=1),axis=1)
+  loss = np.sum(-np.log(z[range(X.shape[0]),y]))
+  loss = loss/X.shape[0]
+  loss += 0.5*reg*np.sum(W*W)
+  # z = z.T
+  grad_1 = np.zeros_like(z)
+  grad_1[range(X.shape[0]),y] = 1 
+  z = z - grad_1
+  dW = np.dot(X.T,z)
+  # print dW.shape
+  dW = dW/X.shape[0]
+  dW += reg*W
   pass
   #############################################################################
   #                          END OF YOUR CODE                                 #
